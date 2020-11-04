4dcdc843f08874d8f0a800cefe9e291e151c096f
J.J.Ding <louis7012161@gmail.com>
2020-11-05 00:52:58 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com> 更新內容: 1.修改字串顯示語法 2.減少全域變數 3.改用List去呈現sorce code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -82,7 +82,6 @@
             this.Controls.Add(this.btnResult);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
