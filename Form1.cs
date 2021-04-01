127276670504a3e920b9872e377a6ed60e7311f8
J.J.Ding <louis7012161@gmail.com>
2021-04-01 23:19:14 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com> Add note

--- Form1.cs
+++ Form1.cs
@@ -1,19 +1,19 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RandomCard
 {
     public partial class Form1 : Form
     {
-        public int cardNumber = 52;//宣告牌數 
+        private int cardNumber = 52;//宣告牌數
+
+        /// <summary>
+        /// 宣告手牌空間
+        /// </summary>
+        private List<int> card = new List<int>();
 
         public Form1()
         {
@@ -22,50 +22,100 @@ namespace RandomCard
             dataGridView1.ColumnCount = 2;
             dataGridView1.Columns[0].Name = "玩家";
             dataGridView1.Columns[1].Name = "手牌";
+            card = getCard();
         }
-
 
         private void btnResult_Click(object sender, EventArgs e)
         {
-            if (dataGridView1.Rows.Count != 0) { dataGridView1.Rows.Clear(); } //DataGridView內容重置(判斷有無內容)
-            int numPlayer = Int32.Parse(txtPlayer.Text);//取得玩家數量
-            List<int> card = new List<int>();//宣告手牌空間
-            if (numPlayer <= 52) {
-                card = getCard();
-                card = Reshuffle(card);
-                int getCardNumber = card.Count / numPlayer;
-                for (int i = 0; i < numPlayer; i++)
-                {
-                    if (card.Count - getCardNumber * i >= getCardNumber)
-                    {
-                        string[] row = new string[2]; //DataGridView 訊息結構   
-                        row[0] = $"玩家{(i + 1)}";
-                        for (int j = (0 + i) * getCardNumber; j < (1 + i) * getCardNumber; j++)
-                        {
-                            row[1] += $"{card[j]},";
-                        }
-                        row[1] += $" 共{getCardNumber}張";
-                        dataGridView1.Rows.Add(row);
-                    }                   
-                }
-                //此區寫剩下的牌數
-                richTextBox1.Clear();
-                if (card.Count % numPlayer != 0)
-                {
-                    string txtMessage = ""; //顯示訊息
-                    txtMessage += $"剩下牌數{card.Count % numPlayer}\n";
-                    for (int h = numPlayer * getCardNumber; h < 52; h++)
-                    {
-                        txtMessage += $"{card[h]},";
-                    }
-                    richTextBox1.AppendText(txtMessage);
-                }
+            //DataGridView內容重置(判斷有無內容)
+
+            List<Class1> aa = new List<Class1>();
+            aa.Add(new Class1("1年A班", "王曉明"));
+            aa.Add(new Class1("1年B班", "鮭魚"));
+            aa.Add(new Class1("1年A班", "小美"));
+            aa.Add(new Class1("1年B班", "天竺鼠車車"));
+
+            var a = aa.Select(data => new
+                                 {
+                                     班級 = $"1{data.班級}",
+                                     班級1 = $"2{data.班級}",
+                                     數字 = int.Parse(data.班級.First().ToString())
+                                 })
+                .First(data=>data.班級1 == "21年B班") ;
+            //等同 var a = aa.Select(data=>$"1{data.班級}").ToList();
+            using (StreamWriter write = new StreamWriter(@".\.txt"))
+            {
+                write.WriteLine("");
+
             }
-            else { MessageBox.Show("玩家太多，且牌不夠!!!"); }
+            try
+            {
 
-         }
+            }
+            catch (Exception ex)
+            { 
+            wr
+            }
 
-        //產生52張牌
+
+            if (dataGridView1.Rows.Count != 0)
+            {
+                dataGridView1.Rows.Clear();
+            }
+
+            if (Int32.TryParse(txtPlayer.Text, out int numPlayer))
+            {
+                //int numPlayer = Int32.Parse(txtPlayer.Text);//取得玩家數量
+
+                if (numPlayer <= cardNumber)
+                {
+                    card = Reshuffle(card);
+
+                    int getCardNumber = card.Count / numPlayer;
+                    for (int i = 0; i < numPlayer; i++)
+                    {
+                        if (card.Count - getCardNumber * i >= getCardNumber)
+                        {
+                            string[] row = new string[2]; //DataGridView 訊息結構
+                            row[0] = $"玩家{(i + 1)}";
+                            for (int j = (0 + i) * getCardNumber; j < (1 + i) * getCardNumber; j++)
+                            {
+                                row[1] += $"{card[j]},";
+                            }
+                            row[1] += $" 共{getCardNumber}張";
+                            dataGridView1.Rows.Add(row);
+                        }
+                    }
+                    //此區寫剩下的牌數
+                    richTextBox1.Clear();
+                    if (card.Count % numPlayer != 0)
+                    {
+                        //20210327
+                        string txtMessage = string.Empty; //顯示訊息
+
+                        txtMessage += $"剩下牌數{card.Count % numPlayer}\n";
+                        for (int h = numPlayer * getCardNumber; h < cardNumber; h++)
+                        {
+                            txtMessage += $"{card[h]},";
+                        }
+                        richTextBox1.AppendText(txtMessage);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("請輸入正確數字");
+                };
+            }
+            else
+            {
+                MessageBox.Show("玩家太多，且牌不夠!!!");
+            }
+        }
+
+        /// <summary>
+        /// 產生52張牌
+        /// </summary>
+        /// <returns></returns>
         public List<int> getCard()
         {
             List<int> card = new List<int>();
@@ -75,14 +125,18 @@ namespace RandomCard
             }
             return card;
         }
-        
-        //以下為洗牌動作
+
+        /// <summary>
+        /// 以下為洗牌動作
+        /// </summary>
+        /// <param name="card"></param>
+        /// <returns></returns>
         public List<int> Reshuffle(List<int> card)
         {
             Random rnd = new Random();//亂數
             List<int> temp = new List<int>();//建立暫存空間
-            for (int i = 52; i > 0; i--)
-            {               
+            for (int i = cardNumber; i > 0; i--)
+            {
                 int c = rnd.Next(0, i);
                 temp.Add(card[c]);
                 card.Remove(card[c]);
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 組件的一般資訊是由下列的屬性集控制。
@@ -22,7 +21,7 @@ using System.Runtime.InteropServices;
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
 [assembly: Guid("3dc8b0be-6b87-48a0-938b-118c56a02c6c")]
 
-// 組件的版本資訊由下列四個值所組成: 
+// 組件的版本資訊由下列四個值所組成:
 //
 //      主要版本
 //      次要版本
